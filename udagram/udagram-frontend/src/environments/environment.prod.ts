bc576ef6cedc7b0d8d2da550bebc708f04beb793
Owen Park <ospark@middlebury.edu>
2026-04-20 17:41:59 -0400

    Fixed api url glitch

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://demo-app-dev3.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://demo-app-dev3.us-east-1.elasticbeanstalk.com/api/v0/feed',
 };
 
 /*
