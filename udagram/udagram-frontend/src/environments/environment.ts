5b6f673ee098c036146853493fbbaf87310bde77
Owen Park <ospark@middlebury.edu>
2026-04-22 12:30:11 -0400

    added link

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://demo-app-dev3.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
